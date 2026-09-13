fdf750c8451f674f07bce7bf6ad491b157e761eb
manuclementz <manu.clementz@gmail.com>
2026-09-13 11:15:44 +0200

    detect door/container activation

--- plugin.cpp
+++ plugin.cpp
@@ -2,6 +2,46 @@ using namespace SKSE;
 using namespace SKSE::log;
 
 namespace {
+    // Fires whenever the player (or anything else) activates a reference in the world -
+    // opening a door, opening a container, talking to an NPC, picking up an item, etc.
+    class ActivationEventSink final : public RE::BSTEventSink<RE::TESActivateEvent> {
+    public:
+        static ActivationEventSink* GetSingleton() {
+            static ActivationEventSink singleton;
+            return &singleton;
+        }
+
+        RE::BSEventNotifyControl ProcessEvent(const RE::TESActivateEvent* a_event,
+                                               RE::BSTEventSource<RE::TESActivateEvent>*) override {
+            if (!a_event || !a_event->objectActivated) {
+                return RE::BSEventNotifyControl::kContinue;
+            }
+
+            auto* refr = a_event->objectActivated.get();
+            auto* baseObject = refr->GetBaseObject();
+            if (!baseObject) {
+                return RE::BSEventNotifyControl::kContinue;
+            }
+
+            if (baseObject->As<RE::TESObjectDOOR>()) {
+                log::info("Door activated: {}", refr->GetName());
+            } else if (baseObject->As<RE::TESObjectCONT>()) {
+                log::info("Container activated: {}", refr->GetName());
+            }
+
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        ActivationEventSink(const ActivationEventSink&) = delete;
+        ActivationEventSink(ActivationEventSink&&) = delete;
+        ActivationEventSink& operator=(const ActivationEventSink&) = delete;
+        ActivationEventSink& operator=(ActivationEventSink&&) = delete;
+
+    private:
+        ActivationEventSink() = default;
+        ~ActivationEventSink() override = default;
+    };
+
     // Initialize spdlog. Writes to a debugger console when attached (e.g. from VS Code/Visual Studio),
     // otherwise writes to Documents/My Games/Skyrim Special Edition/SKSE/<PluginName>.log
     void InitializeLogging() {
@@ -34,6 +74,8 @@ namespace {
                         // All ESM/ESL/ESP plugins have loaded and the main menu is active.
                         // It's now safe to look up forms, records, etc.
                         log::info("Data loaded.");
+                        RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESActivateEvent>(
+                            ActivationEventSink::GetSingleton());
                         break;
                     case MessagingInterface::kNewGame:
                         log::info("New game started.");
